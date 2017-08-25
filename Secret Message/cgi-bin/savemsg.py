4e25e2e3cd08ab38a901490d6f725114b52a87e3
Max <MaxMB15@gmail.com>
2017-08-25 13:32:17 -0400

    added html handling and text file appending

--- Secret Message/cgi-bin/savemsg.py
+++ Secret Message/cgi-bin/savemsg.py
@@ -1,1 +1,12 @@
-import cgi
+import cgi, cgitb, urllib2
+from subprocess import call
+
+
+"""get the input from the html file"""
+form = cgi.FieldStorage()
+inputString = form.getvalue('MSG')
+
+"""save it to a file"""
+textFile = open("data.txt", "a")
+textFile.write("Test\n")
+textFile.close()
