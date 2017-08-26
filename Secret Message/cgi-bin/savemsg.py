74f1453e372add854d0ef9ae7c069c2c4b618f20
Max <MaxMB15@gmail.com>
2017-08-26 15:49:57 -0400

    Text sent gets inputed into a text file

--- Secret Message/cgi-bin/savemsg.py
+++ Secret Message/cgi-bin/savemsg.py
@@ -7,14 +7,15 @@ from subprocess import call
 """get the input from the html file"""
 form = cgi.FieldStorage()
 inputString = form.getvalue('MSG')
-inputString = inputString + ''
 
 """save it to a file"""
 textFile = open("data.txt", "a")
-textFile.write("Test\n")
+textFile.write("%s\n" %inputString)
 textFile.close
 
 print "Content-type:text/html\r\n\r\n"
 print '<html>'
-print '%s' %inputString
+print '<center><h3>'
+print 'Your message has been sent. <3'
+print '</h3></center>'
 print '</html>'
