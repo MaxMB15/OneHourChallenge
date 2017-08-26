c6382da8a99a81934d9677163a8c1f999b478692
Max <MaxMB15@gmail.com>
2017-08-26 11:00:24 -0400

    some bug fixes to savemsg.py to stop the server error

--- Secret Message/cgi-bin/savemsg.py
+++ Secret Message/cgi-bin/savemsg.py
@@ -5,8 +5,14 @@ from subprocess import call
 """get the input from the html file"""
 form = cgi.FieldStorage()
 inputString = form.getvalue('MSG')
+inputString = inputString + ''
 
 """save it to a file"""
 textFile = open("data.txt", "a")
 textFile.write("Test\n")
-textFile.close()
+textFile.close
+
+print 'Content-Type:text/html\n\n'
+print '<html>'
+print '%s' %inputString
+print '</html>'
